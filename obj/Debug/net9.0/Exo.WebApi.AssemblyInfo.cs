67cb8ce134ed1bbfcd553b3d5a1473fdba2e36cc
LucasLeonardoGuedes <lucas8cef20@gmail.com>
2025-09-23 14:00:23 -0300

    Atualização de complemento do método CRUD

--- obj/Debug/net9.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4cfd684f63ac091bb1559d30ae83f854ae5d33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
